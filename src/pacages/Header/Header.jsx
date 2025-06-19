fee9ac47e2694fd3c1dcd0b453fe6ea6438f6733
Bovt Dmytro <mr.bovt2004@gmail.com>
2025-06-19 17:48:56 +0300

    comit

--- src/pacages/Header/Header.jsx
+++ src/pacages/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 export default function Header() {
   return (
-    <header >
+    <header>
       <div className="header">
         <div className="header_logo">
           <img
@@ -14,21 +15,21 @@ export default function Header() {
         </div>
 
         <div className="header_buttons">
-        <div className="header_buttons-button">
-          <a href="./" className="header_buttons-button-text">
-            HOME
-          </a>
-        </div>
-        <div className="header_buttons-button">
-          <a href="staff" className="header_buttons-button-text">
-            STAFF
-          </a>
-        </div>
-        <div className="header_buttons-button">
-          <a href="reserv" className="header_buttons-button-text">
-            RESERV
-          </a>
-        </div>
+          <div className="header_buttons-button">
+            <Link to="/" className="header_buttons-button-text">
+              HOME
+            </Link>
+          </div>
+          <div className="header_buttons-button">
+            <Link to="/staff" className="header_buttons-button-text">
+              STAFF
+            </Link>
+          </div>
+          <div className="header_buttons-button">
+            <Link to="/reserv" className="header_buttons-button-text">
+              RESERV
+            </Link>
+          </div>
         </div>
 
         <div className="header_phones">
